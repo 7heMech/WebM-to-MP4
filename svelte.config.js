f2c62192440fd75f531c8fca66bb1322d3211aad
7heMech <83923848+7heMech@users.noreply.github.com>
2023-12-13 12:23:24 +0000

    try to use custom headers

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-      pages: 'build',
-      assets: 'build',
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
       fallback: null,
       precompress: false,
-      strict: true
-    })
-	},
+      strict: true,
+    }),
+  },
 };
 
 export default config;
